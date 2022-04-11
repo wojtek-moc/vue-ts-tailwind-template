77a0841ff7431f726594b2490ce0c832752e491e
Wojciech Mocarski <wojciech.mocarski@gmail.com>
2022-04-11 23:37:29 +0200

    Template configuration

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="flex flex-col h-96 justify-center items-center">
+    <img class="w-32 h-32" alt="Vue logo" src="./assets/logo.png" />
+    <h1 class="text-xl font-semibold">My template with:</h1>
+    <HelloWorld msg="Vue 3 + TypeScript + Vite + Tailwindcss" />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
